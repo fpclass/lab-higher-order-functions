6d15b12b71d9914e55541121fe432e1abda568f9
Michael Gale <m.gale@warwick.ac.uk>
2019-12-31 20:58:12 +0000

    Lab3 => Higher-order functions

--- src/Lab.hs
+++ src/Lab.hs
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 -- Functional Programming (CS141)                                             --
--- Lab 3: Recursive and higher-order functions                                --
+-- Lab: Higher-order functions                                                --
 --------------------------------------------------------------------------------
 
 module Lab where
@@ -10,7 +10,7 @@ module Lab where
 -- Some of the functions we will be defining as part of this lab are
 -- part of Haskell's standard library. The following line tells the compiler
 -- not to import them.
-import Prelude hiding ( Monoid(..), elem, maximum, intersperse, transpose
+import Prelude hiding ( elem, maximum, intersperse, transpose
                       , subsequences, permutations, any, all, flip, takeWhile
                       , zipWith, groupBy, notElem )
 
@@ -22,9 +22,6 @@ elem = undefined
 
 maximum :: Ord a => [a] -> a
 maximum = undefined
-
-intersperse :: a -> [a] -> [a]
-intersperse = undefined
 
 any :: (a -> Bool) -> [a] -> Bool
 any = undefined
@@ -44,38 +41,7 @@ zipWith = undefined
 groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
 groupBy = undefined
 
-subsequences :: [a] -> [[a]]
-subsequences = undefined
-
 permutations :: Eq a => [a] -> [[a]]
 permutations = undefined
-
---------------------------------------------------------------------------------
--- Monoids
-
--- Monoid laws:
---
--- (Left identity)      mappend mempty x = x
--- (Right identity)     mappend x mempty = x
--- (Associativity)      mappend x (mappend y z) = mappend (mappend x y) z
--- (mconcat)            mconcat = foldr mappend mempty
-
-class Monoid a where
-    mempty  :: a
-    mappend :: a -> a -> a
-    mconcat :: [a] -> a
-    mconcat = undefined
-
-instance Monoid Int where
-    mempty  = undefined
-    mappend = undefined
-
-instance Monoid [a] where
-    mempty  = undefined
-    mappend = undefined
-
-instance Monoid b => Monoid (a -> b) where
-    mempty  = undefined
-    mappend = undefined
 
 --------------------------------------------------------------------------------
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 -- Functional Programming (CS141)                                             --
--- Lab 3: Recursive and higher-order functions                                --
+-- Lab: Higher-order functions                                                --
 --------------------------------------------------------------------------------
 
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -9,17 +9,11 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
-import Data.List (nub,intersperse,subsequences,groupBy,sort)
+import Data.List (nub,subsequences,groupBy,sort)
 
 import qualified Lab as L
 
 --------------------------------------------------------------------------------
-
-separatedBy :: Eq a => a -> [a] -> Bool
-separatedBy s []       = True
-separatedBy s [y]      = True
-separatedBy s (_:y:xs) =
-    not (null xs) && y == s && separatedBy s xs
 
 fac :: Int -> Int
 fac 0 = 1
@@ -35,10 +29,6 @@ main = hspec $ do
         prop "finds the maximum in non-empty lists" $
             forAll (listOf1 arbitrary) $ \(xs :: [Int]) ->
             L.maximum xs == last (sort xs)
-    describe "intersperse" $
-        prop "separates elements of a list with some value" $
-            \(xs :: [Char]) (x :: Char) ->
-            separatedBy x (L.intersperse x xs)
     describe "any" $ do
         prop "determines whether at least one element satisfies the predicate" $
             \(xs :: [Int]) -> L.any odd xs == not (null (filter odd xs))
@@ -64,12 +54,6 @@ main = hspec $ do
             \(xs :: [Int]) -> and [all (==x) xs | (x:xs) <- L.groupBy (==) xs]
         prop "the sum of lengths of the sublists is the length of the input" $
             \(xs :: [Int]) -> length xs == sum (map length $ L.groupBy (==) xs)
-    describe "subsequences" $ modifyMaxSize (const 10) $ do
-        prop "produces the right number of subsequences" $ \(xs :: [Int]) ->
-            length (L.subsequences xs) == 2^(length xs)
-        prop "produces unique subsequences" $ \(xs :: [Int]) ->
-            length (L.subsequences (nub xs)) ==
-            length (nub (L.subsequences (nub xs)))
     describe "permutations" $ modifyMaxSize (const 10) $ do
         prop "produces the right number of permutations" $ \(xs :: [Int]) ->
             length (L.permutations xs) == fac (length xs)
@@ -79,23 +63,5 @@ main = hspec $ do
         prop "permutations are all the same length as the input" $
             \(xs :: [Int]) ->
                 all ((==) (length xs) . length) (L.permutations xs)
-    describe "Monoid instance for Int" $ do
-        prop "satisfies the left identity" $ \(x :: Int) ->
-            L.mappend L.mempty x == x
-        prop "satisfies the right identity" $ \(x :: Int) ->
-            L.mappend x L.mempty == x
-        prop "mappend is associative" $ \(x :: Int) y z ->
-            L.mappend x (L.mappend y z) == L.mappend (L.mappend x y) z
-        prop "mconcat concatenates" $ \(xs :: [Int]) ->
-            L.mconcat xs == foldr L.mappend L.mempty xs
-    describe "Monoid instance for [a]" $ do
-        prop "satisfies the left identity" $ \(x :: [Int]) ->
-            L.mappend L.mempty x == x
-        prop "satisfies the right identity" $ \(x :: [Int]) ->
-            L.mappend x L.mempty == x
-        prop "mappend is associative" $ \(x :: [Int]) y z ->
-            L.mappend x (L.mappend y z) == L.mappend (L.mappend x y) z
-        prop "mconcat concatenates" $ \(xs :: [[Int]]) ->
-            L.mconcat xs == foldr L.mappend L.mempty xs
 
 --------------------------------------------------------------------------------
